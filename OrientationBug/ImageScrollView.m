d30e5b16de9ada825d03dee5242ddb8be23e975d
Rob Mayoff <git@rob.dqd.com>
2014-09-25 12:07:26 -0500

    fix removeExtraSubviews to removeFromSuperview

--- OrientationBug/ImageScrollView.m
+++ OrientationBug/ImageScrollView.m
@@ -1,110 +1,111 @@
-//
-//  ImageScrollView.m
-//  OrientationBug
-//
-//  Created by Rob Mayoff on 9/25/14.
-//  Copyright (c) 2014 Phillip. All rights reserved.
-//
+    //
+    //  ImageScrollView.m
+    //  OrientationBug
+    //
+    //  Created by Rob Mayoff on 9/25/14.
+    //  Copyright (c) 2014 Phillip. All rights reserved.
+    //
 
-#import "ImageScrollView.h"
-#import <tgmath.h>
+    #import "ImageScrollView.h"
+    #import <tgmath.h>
 
-@implementation ImageScrollView {
-    NSMutableArray *imageSubviews;
-    CGSize priorSize;
-    CGFloat pageNumber;
-    BOOL needsToSyncSubviewsWithImages : 1;
-}
-
-#pragma mark - Public API
-
-@synthesize images = _images;
-
-- (void)setImages:(NSArray *)images {
-    _images = [images copy];
-    needsToSyncSubviewsWithImages = YES;
-}
-
-#pragma mark - UIView overrides
-
-- (void)layoutSubviews {
-    [super layoutSubviews];
-    if (needsToSyncSubviewsWithImages) {
-        [self syncSubviewsWithImages];
+    @implementation ImageScrollView {
+        NSMutableArray *imageSubviews;
+        CGSize priorSize;
+        CGFloat pageNumber;
+        BOOL needsToSyncSubviewsWithImages : 1;
     }
-    if (needsToSyncSubviewsWithImages || !CGSizeEqualToSize(self.bounds.size, priorSize)) {
-        [self layoutForNewSize];
+
+    #pragma mark - Public API
+
+    @synthesize images = _images;
+
+    - (void)setImages:(NSArray *)images {
+        _images = [images copy];
+        needsToSyncSubviewsWithImages = YES;
     }
-    needsToSyncSubviewsWithImages = NO;
-    priorSize = self.bounds.size;
-    [self updatePageNumber];
-}
 
-#pragma mark - Implementation details
+    #pragma mark - UIView overrides
 
-- (void)syncSubviewsWithImages {
-    [self ensureImageSubviewsArrayExists];
-    [self putImagesInSubviews];
-    [self removeExtraSubviews];
-}
-
-- (void)ensureImageSubviewsArrayExists {
-    if (imageSubviews == nil) {
-        imageSubviews = [NSMutableArray arrayWithCapacity:self.images.count];
+    - (void)layoutSubviews {
+        [super layoutSubviews];
+        if (needsToSyncSubviewsWithImages) {
+            [self syncSubviewsWithImages];
+        }
+        if (needsToSyncSubviewsWithImages || !CGSizeEqualToSize(self.bounds.size, priorSize)) {
+            [self layoutForNewSize];
+        }
+        needsToSyncSubviewsWithImages = NO;
+        priorSize = self.bounds.size;
+        [self updatePageNumber];
     }
-}
 
-- (void)putImagesInSubviews {
-    [self.images enumerateObjectsUsingBlock:^(id obj, NSUInteger i, BOOL *stop) {
-        [self putImage:obj inSubviewAtIndex:i];
-    }];
-}
+    #pragma mark - Implementation details
 
-- (void)removeExtraSubviews {
-    if (imageSubviews.count > self.images.count) {
-        [imageSubviews removeObjectsInRange:NSMakeRange(self.images.count, imageSubviews.count - self.images.count)];
+    - (void)syncSubviewsWithImages {
+        [self ensureImageSubviewsArrayExists];
+        [self putImagesInSubviews];
+        [self removeExtraSubviews];
     }
-}
 
-- (void)putImage:(UIImage *)image inSubviewAtIndex:(NSUInteger)i {
-    UIImageView *imageView = [self imageViewAtIndex:i];
-    imageView.image = image;
-}
-
-- (UIImageView *)imageViewAtIndex:(NSUInteger)i {
-    while (i >= imageSubviews.count) {
-        UIView *view = [[UIImageView alloc] init];
-        view.contentMode = UIViewContentModeScaleAspectFit;
-        view.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin;
-        [self addSubview:view];
-        [imageSubviews addObject:view];
+    - (void)ensureImageSubviewsArrayExists {
+        if (imageSubviews == nil) {
+            imageSubviews = [NSMutableArray arrayWithCapacity:self.images.count];
+        }
     }
-    return imageSubviews[i];
-}
 
-- (void)layoutForNewSize {
-    [self setSubviewFramesAndContentSize];
-    [self alignToNearestPage];
-}
-
-- (void)setSubviewFramesAndContentSize {
-    CGRect frame = self.bounds;
-    frame.origin = CGPointZero;
-    for (UIView *subview in imageSubviews) {
-        subview.frame = frame;
-        frame.origin.x += frame.size.width;
+    - (void)putImagesInSubviews {
+        [self.images enumerateObjectsUsingBlock:^(id obj, NSUInteger i, BOOL *stop) {
+            [self putImage:obj inSubviewAtIndex:i];
+        }];
     }
-    self.contentSize = CGSizeMake(frame.origin.x, frame.size.height);
-}
 
-- (void)alignToNearestPage {
-    self.contentOffset = CGPointMake(pageNumber * self.bounds.size.width, 0);
-}
+    - (void)removeExtraSubviews {
+        while (imageSubviews.count > self.images.count) {
+            [imageSubviews.lastObject removeFromSuperview];
+            [imageSubviews removeLastObject];
+        }
+    }
 
-- (void)updatePageNumber {
-    // Note that self.contentOffset == self.bounds.origin.
-    CGRect bounds = self.bounds;
-    pageNumber = fmin(round(bounds.origin.x / bounds.size.width), self.images.count - 1);
-}
+    - (void)putImage:(UIImage *)image inSubviewAtIndex:(NSUInteger)i {
+        UIImageView *imageView = [self imageViewAtIndex:i];
+        imageView.image = image;
+    }
 
-@end
+    - (UIImageView *)imageViewAtIndex:(NSUInteger)i {
+        while (i >= imageSubviews.count) {
+            UIView *view = [[UIImageView alloc] init];
+            view.contentMode = UIViewContentModeScaleAspectFit;
+            view.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin;
+            [self addSubview:view];
+            [imageSubviews addObject:view];
+        }
+        return imageSubviews[i];
+    }
+
+    - (void)layoutForNewSize {
+        [self setSubviewFramesAndContentSize];
+        [self alignToNearestPage];
+    }
+
+    - (void)setSubviewFramesAndContentSize {
+        CGRect frame = self.bounds;
+        frame.origin = CGPointZero;
+        for (UIView *subview in imageSubviews) {
+            subview.frame = frame;
+            frame.origin.x += frame.size.width;
+        }
+        self.contentSize = CGSizeMake(frame.origin.x, frame.size.height);
+    }
+
+    - (void)alignToNearestPage {
+        self.contentOffset = CGPointMake(pageNumber * self.bounds.size.width, 0);
+    }
+
+    - (void)updatePageNumber {
+        // Note that self.contentOffset == self.bounds.origin.
+        CGRect bounds = self.bounds;
+        pageNumber = fmin(round(bounds.origin.x / bounds.size.width), self.images.count - 1);
+    }
+
+    @end
